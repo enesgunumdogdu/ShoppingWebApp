9a900a4f532e12a1a2ddb87bca4ac37c61997e42
A. Enes Günümdoğdu <enesgunumdogdu0@gmail.com>
2024-03-09 00:00:27 +0300

    DbContext added.

---
+++ ShoppingWebApp.Services.CouponAPI/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingWebApp.Services.CouponAPI.Models;
+
+namespace ShoppingWebApp.Services.CouponAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Coupon> Coupons{ get; set; }
+    }
+}
--- ShoppingWebApp.Services.CouponAPI/Models/Coupon.cs
+++ ShoppingWebApp.Services.CouponAPI/Models/Coupon.cs
@@ -1,11 +1,15 @@
-﻿namespace ShoppingWebApp.Services.CouponAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingWebApp.Services.CouponAPI.Models
 {
     public class Coupon
     {
+        [Key]
         public int CouponId { get; set; }
+        [Required]
         public string CouponCode { get; set; }
+        [Required]
         public double DiscountAmount { get; set; }
         public int MinAmount { get; set; }
-        public DateTime LastUpdated { get; set; }
     }
 }

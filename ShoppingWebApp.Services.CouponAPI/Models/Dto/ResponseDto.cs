ca67497253f42c57fed615f64182ce8f2219f50e
A. Enes Günümdoğdu <enesgunumdogdu0@gmail.com>
2024-03-10 23:59:44 +0300

    ResponseDto created, automapper added.

---
+++ ShoppingWebApp.Services.CouponAPI/Models/Dto/ResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ShoppingWebApp.Services.CouponAPI.Models.Dto
+{
+    public class ResponseDto
+    {
+        public object? Result { get; set; }
+        public bool IsSuccess { get; set; } = true;
+        public string Message { get; set; } = "";
+
+    }
+}
--- ShoppingWebApp.Services.CouponAPI/Program.cs
+++ ShoppingWebApp.Services.CouponAPI/Program.cs
@@ -1,4 +1,6 @@
+using AutoMapper;
 using Microsoft.EntityFrameworkCore;
+using ShoppingWebApp.Services.CouponAPI;
 using ShoppingWebApp.Services.CouponAPI.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +10,10 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle

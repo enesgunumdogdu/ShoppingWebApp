4960c772c95f87cc2833ef6ba5f4623b6b68d2ea
A. Enes Günümdoğdu <enesgunumdogdu0@gmail.com>
2024-03-10 23:28:50 +0300

    ApplyMigration method added. Sample coupons added.

--- ShoppingWebApp.Services.CouponAPI/Program.cs
+++ ShoppingWebApp.Services.CouponAPI/Program.cs
@@ -30,3 +30,18 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
+// checks if there is any pending migrations, if there is then it migrates.
+void ApplyMigration()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var _db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+        if(_db.Database.GetPendingMigrations().Count() > 0)
+        {
+            _db.Database.Migrate();
+        }
+    }
+}

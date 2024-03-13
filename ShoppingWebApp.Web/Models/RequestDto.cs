8ea99a59d1b1d5f1f0b3235509f738276be6083f
A. Enes Günümdoğdu <enesgunumdogdu0@gmail.com>
2024-03-13 15:34:32 +0300

    RequestDto & ResponseDto added.

---
+++ ShoppingWebApp.Web/Models/RequestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ShoppingWebApp.Web.Models
+{
+    public class RequestDto
+    {
+        public string ApiType { get; set; } = "GET";
+        public string Url {  get; set; }
+        public string Data {  get; set; }
+        public string AccessToken {  get; set; }
+    }
+}

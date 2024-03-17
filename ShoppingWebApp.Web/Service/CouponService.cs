2a512d27036fd18759afa28e79f0f4c2ceaf3b94
A. Enes Günümdoğdu <enesgunumdogdu0@gmail.com>
2024-03-17 17:05:00 +0300

    CouponService Added.

---
+++ ShoppingWebApp.Web/Service/CouponService.cs
@@ -0,0 +1,44 @@
+﻿using ShoppingWebApp.Web.Models;
+using ShoppingWebApp.Web.Service.IService;
+
+namespace ShoppingWebApp.Web.Service
+{
+    public class CouponService : ICouponService
+    {
+        private readonly IBaseService _baseService;
+        public CouponService(IBaseService baseService)
+        {
+            _baseService = baseService;
+        }
+
+        public Task<ResponseDto?> CreateCouponAsync(CouponDto couponDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto?> DeleteCouponAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto?> GetAllCouponsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto?> GetCoupon(string CouponCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto?> GetCouponById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto?> UpdateCouponAsync(CouponDto couponDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
